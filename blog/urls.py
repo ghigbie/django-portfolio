34c219849d34f6720e3214b50adc3058895d475a
ghigbie <gehigbie@gmail.com>
2019-07-14 13:49:30 -0600

    Edit url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.allblogs, name="allblogs"),
-<<<<<<< HEAD
-    path('<int:blog_id>/', views.blog_detail, name="blog_detail"),
-=======
-    path('<int:blog_id>/', views.detail, name='detail'),
->>>>>>> 8bc3d9c71039f5f5d7f427a49f538889a9175996
+    path('<int:blog_id>/', views.detail, name="detail"),
 ]
