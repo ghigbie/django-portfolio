8bc3d9c71039f5f5d7f427a49f538889a9175996
George Higbie <ghigbie@monigle.com>
2019-07-13 19:50:15 -0600

    Edit paths and rendering

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
     blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', 
+    return render(request, 'blog/blogs.html', 
                   {'title': 'Doggie Blog',
                    'blogs': blogs})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog' : detail_blog})
+    
