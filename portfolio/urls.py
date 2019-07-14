218c34257375631a39e437e48ab4720fb1a561c6
ghigbie <gehigbie@gmail.com>
2019-07-14 14:06:29 -0600

    Add static files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,5 +8,5 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')), #this line forwards to the url for blogs
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
