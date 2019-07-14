698c9d439d2709b57e549d625ae5329c17951a9d
ghigbie <gehigbie@gmail.com>
2019-07-14 13:34:44 -0600

    Add route

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name="allblogs")
+    path('', views.allblogs, name="allblogs"),
+    path('<int:blog_id>/'), view.blog_detail.html, name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
+blogs = Blog.objects
+
 def allblogs(request):
-    blogs = Blog.objects
     return render(request, 'blog/allblogs.html', 
                   {'title': 'Doggie Blog',
                    'blogs': blogs})
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blog_detail.html',
+                  {'title' : 'Doggie Blog Detail',
+                   'blog' : blog})
+    
